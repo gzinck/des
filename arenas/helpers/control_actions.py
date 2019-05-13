ea1e5aacae87565416010eea453f2a2f9683b274
Graeme Zinck <graeme.zinck@gmail.com>
2019-05-13 15:28:22 +0200

    Modified the appropriate control actions so that all of them contain the uncontrollable actions

--- arenas/helpers/control_actions.py
+++ arenas/helpers/control_actions.py
@@ -6,6 +6,8 @@ def get_valid_control_actions(automaton, state):
     """This gets all possible valid control actions for an automaton at a given
     state. That is, it finds all possible actions for the automaton from the
     current state and returns its power set as a list of tuples.
+    Note that if there exist uncontrollable events that are defined, they must
+    be part of each control action returned.
 
     Notes
     -----
@@ -27,17 +29,27 @@ def get_valid_control_actions(automaton, state):
     Examples
     --------
     >>> print(get_valid_control_actions(automaton, "{q0, q1, q2}"))
-    [["a"], ["b"], ["a", "b"]]
+    [["a", "u"], ["b", "u"], ["a", "b", "u"]]
     """
-    all_events = automaton["events"]["all"]  # TODO: SHOULD THIS BE CHANGED?
-    valid_events = []
-    for event in all_events:
+    # Check for valid controllable events for the controller
+    cont_events = automaton["events"]["controllable"][0]
+    valid_cevents = []
+    for event in cont_events:
         trans = str_helper.format_transition(state, event)
         if trans in automaton["transitions"]["all"]:
-            valid_events.append(event)
+            valid_cevents.append(event)
+
+    # Check for valid uncontrollable events
+    uncont_events = set(automaton["events"]["all"]).difference(set(cont_events))
+    valid_ucevents = []
+    for event in uncont_events:
+        trans = str_helper.format_transition(state, event)
+        if trans in automaton["transitions"]["all"]:
+            valid_ucevents.append(event)
     # Now that we have all events, get the power set
     all_actions = list(chain.from_iterable(
-        combinations(valid_events, x) for x in range(1, len(valid_events) + 1)
+        combinations(valid_cevents, x) for x in range(0, len(valid_cevents) + 1)
     ))
     # Convert to lists
-    return [list(x) for x in all_actions]
+    events = [list(x) + valid_ucevents for x in all_actions]
+    return [x for x in events if len(x) > 0]
