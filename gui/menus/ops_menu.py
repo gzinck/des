8471ce2f57f9dd3a0025bcd9203435c94360bee9
Graeme Zinck <graeme.zinck@gmail.com>
2019-06-18 09:03:51 +0200

    Determinize method now works in the GUI.

--- gui/menus/ops_menu.py
+++ gui/menus/ops_menu.py
@@ -2,7 +2,11 @@ from kivy.app import App
 from kivy.uix.screenmanager import Screen
 from gui.selection.single_automaton_selector import SingleAutomatonSelector
 from gui.selection.observer_selector import ObserverSelector
+from gui.popups.name_automaton_popup import NameAutomatonPopup
 from kivy.properties import ObjectProperty
+
+# Operations
+from basic_ops.determinize import determinize
 
 
 class OpsMenu(Screen):
@@ -11,10 +15,10 @@ class OpsMenu(Screen):
     def determinize(self):
         # SEQUENCE: get single automaton, get observer, perform action
         popup = SingleAutomatonSelector()
-        popup.bind(on_dismiss=self.get_observer)
+        popup.bind(on_dismiss=self.det_get_observer)
         popup.open()
 
-    def get_observer(self, instance):
+    def det_get_observer(self, instance):
         name = instance.selected
         app = App.get_running_app()
         selected = [x for x in app.open_automata if x["name"] == name]
@@ -22,7 +26,14 @@ class OpsMenu(Screen):
             selected = selected[0]
             print("Selected the automaton...")
             popup = ObserverSelector(selected)
+            popup.bind(on_dismiss=lambda obs_selector:
+                       self.det_perform_operation(obs_selector, selected))
             popup.open()
 
-    def perform_operation(self):
-        pass
+    def det_perform_operation(self, instance, automaton):
+        observer = instance.selected
+        if len(observer) != 0:
+            observer = observer[0]
+            new_automaton = determinize(automaton, observer)
+            popup = NameAutomatonPopup(automaton=new_automaton)
+            popup.open()
